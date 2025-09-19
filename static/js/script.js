0a013959ec433112b7292f014e87b5c36cf54eb1
luisamichelinr <luisa.michelinr@gmail.com>
2025-09-19 16:54:12 -0300

    Cards dos produtos no mobile

--- static/js/script.js
+++ static/js/script.js
@@ -2,11 +2,12 @@ $(document).ready(function(){
     $('.carrossel').owlCarousel({
         loop: true,                  // Permite looping infinito
         margin: 10,                  // Margem entre os itens
+        nav: true,                  // Não tem setas de navegação
         responsiveClass: true,       // Ajuste de responsividade
         autoplay: true,              // Ativa o autoplay
         autoplayTimeout: 3000,       // Tempo de troca automática (3000ms = 3 segundos)
         autoplayHoverPause: true,    // Pausa o autoplay quando o mouse está sobre o carrossel
-        items: 2,                    
+        items: 1,                    
         center: true,                // Centraliza os itens
         autoWidth: false             // Não permite largura automática
     });
