0aead9afa58d5d5dd3ba4bfe7677700a276b99e2
luisamichelinr <luisa.michelinr@gmail.com>
2025-09-19 14:57:16 -0300

    Carrossel
    Finalização do carrossel simples

--- static/js/script.js
+++ static/js/script.js
@@ -1,46 +1,13 @@
 $(document).ready(function(){
-    $(".carrossel-esquerda").owlCarousel({
-        loop: true,
-        dots: true,
-        margin: 30,
-        autoplay: true,
-        autoplayTimeout: 5000,
-        autoplaySpeed: 2000,
-        slideTransition: 'linear',
-        autoplayHoverPause: true,
-        responsive:{
-            0:{
-                items: 1
-            },
-            600:{
-                items: 2
-            },
-            1000:{
-                items: 2
-            }
-        }
-    });
-
-    $(".carrossel-direita").owlCarousel({
-        loop: true,
-        dots: true,
-        margin: 30,
-        autoplay: true,
-        autoplayTimeout: 5000,
-        autoplaySpeed: 2000,
-        slideTransition: 'linear',
-        autoplayHoverPause: true,
-        rtl: true,
-        responsive:{
-            0:{
-                items: 1
-            },
-            600:{
-                items: 2
-            },
-            1000:{
-                items: 2
-            }
-        }
+    $('.carrossel').owlCarousel({
+        loop: true,                  // Permite looping infinito
+        margin: 10,                  // Margem entre os itens
+        responsiveClass: true,       // Ajuste de responsividade
+        autoplay: true,              // Ativa o autoplay
+        autoplayTimeout: 3000,       // Tempo de troca automática (3000ms = 3 segundos)
+        autoplayHoverPause: true,    // Pausa o autoplay quando o mouse está sobre o carrossel
+        items: 2,                    
+        center: true,                // Centraliza os itens
+        autoWidth: false             // Não permite largura automática
     });
 });
