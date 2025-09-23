d19997ce8872bb11fcfab55109106d98304951c2
Luísa Michelin Riqueti <luisa.michelinr@gmail.com>
2025-09-23 07:19:26 -0300

    Javascript para adicionar insumo

--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,39 @@
-$(document).ready(function(){
-    $('.carrossel').owlCarousel({
-        loop: true,                  // Permite looping infinito
-        margin: 10,                  // Margem entre os itens
-        nav: true,                  // Não tem setas de navegação
-        responsiveClass: true,       // Ajuste de responsividade
-        autoplay: true,              // Ativa o autoplay
-        autoplayTimeout: 3000,       // Tempo de troca automática (3000ms = 3 segundos)
-        autoplayHoverPause: true,    // Pausa o autoplay quando o mouse está sobre o carrossel
-        items: 1,                    
-        center: true,                // Centraliza os itens
-        autoWidth: false             // Não permite largura automática
-    });
+// Script para mostrar/ocultar respostas no FAQ
+function mostrarResposta(id) {
+    if (document.getElementById(id).style.display === 'block') {
+        document.getElementById(id).style.display = 'none'
+    }
+    else {
+        document.getElementById(id).style.display = 'block'
+    }
+}
+
+// Script para atualizar o valor da barra de lucro
+var porcentagem = document.getElementById("lucro");
+
+porcentagem.addEventListener("input", function() {
+    var lucro = document.getElementById("lucro");
+    document.getElementById("valor").innerText = lucro.value + "%";
 });
+
+// Script para mostrar/ocultar card de adição de insumo
+function adicionarInsumo() {
+    if (document.getElementById("adicionarInsumo").style.display === 'block') {
+        document.getElementById("adicionarInsumo").style.display = 'none';
+    }
+    else {
+        document.getElementById("adicionarInsumo").style.display = 'block';
+    }
+}
+
+// Script para mostrar/ocultar campos de quantidade dos ingredientes
+function mostrarQuantidade(id) {
+    if (document.getElementById(id).style.display === 'flex') {
+        document.getElementById(id).style.display = 'none'
+    }
+    else {
+        document.getElementById(id).style.display = 'flex'
+    }
+}
+
+
